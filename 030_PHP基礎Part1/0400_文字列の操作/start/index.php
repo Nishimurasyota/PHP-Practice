37fff533047524b2cb2d5d09fbbd4021463ea7be
nishimurasyota <syota4723@gmail.com>
2022-02-09 21:47:31 +0900

    文字列の操作について

--- 030_PHP基礎Part1/0400_文字列の操作/start/index.php
+++ 030_PHP基礎Part1/0400_文字列の操作/start/index.php
@@ -1,1 +1,11 @@
-<?php 
+<?php
+$name = 'kanon';
+$personName = "syota";
+
+echo 'Hello,' . $name . '<br>';
+// .で文字列を結合する
+// ""で囲った文字列内では変数が使える、エスケープシーケンスが使用できる
+echo "Hello, $name\n";
+
+
+echo "Hello, $personName";
