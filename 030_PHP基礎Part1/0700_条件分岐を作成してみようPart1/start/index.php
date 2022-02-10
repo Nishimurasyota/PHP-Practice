7dc0b7bee60bbb584f8dfcc6bcacc3ab17540fd7
nishimurasyota <syota4723@gmail.com>
2022-02-10 19:34:12 +0900

    if文について

--- 030_PHP基礎Part1/0700_条件分岐を作成してみようPart1/start/index.php
+++ 030_PHP基礎Part1/0700_条件分岐を作成してみようPart1/start/index.php
@@ -1,1 +1,14 @@
-<?php 
+<?php
+
+$testValue = 69;
+
+if ($testValue < 50) {
+    echo "不合格";
+} elseif ($testValue >= 50 && $testValue < 70) {
+    echo "合格";
+} else {
+    echo "秀";
+}
+
+//&&で２つの条件を満たす場合はtrueになる
+//||で２つの条件のうち片方を満たす場合はtrueになる
