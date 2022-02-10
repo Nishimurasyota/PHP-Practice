f41f5a2f1210e3de803a8c44b055160b6291fff8
nishimurasyota <syota4723@gmail.com>
2022-02-10 19:46:07 +0900

    等価性について

--- 030_PHP基礎Part1/0800_条件分岐を作成してみようPart2/start/index.php
+++ 030_PHP基礎Part1/0800_条件分岐を作成してみようPart2/start/index.php
@@ -1,1 +1,26 @@
-<?php 
+<?php
+
+if (1 == "1") {
+    echo "true";
+} else {
+    echo "false";
+}
+?>
+<br>
+<?php
+if (1 === "1") {
+    echo "true";
+} else {
+    echo "false";
+}
+
+
+//===の時は型が同じどうかも確認する、基本は===が望ましい
+
+//falsyな値
+/*
+"" (空文字)
+0 (数値、文字列)
+null
+false
+*/
