914f0316f6fc2baaef762c3da908a5822d7ed1da
nishimurasyota <syota4723@gmail.com>
2022-02-13 17:30:55 +0900

    配列の確認問題

--- 040_PHP基礎Part2/0400_理解度チェック（配列）/start/index.php
+++ 040_PHP基礎Part2/0400_理解度チェック（配列）/start/index.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * 商品名 => [価格, 在庫数]
  */
 $products = [
-    'table' => [1000, 2], 
+    'table' => [1000, 2],
     'chair' => [500, 4],
     'bed' => [10000, 1],
     'light' => [5000, 1]
@@ -19,9 +20,13 @@ $products = [
  * lightは5000円で1個存在します。
  */
 echo '<div>商品一覧</div>';
+foreach ($products as $p_name => $val) {
+    $p_price = $val[0];
+    $P_count = $val[1];
+    echo "<div>{$p_name}は{$p_price}円で{$P_count}個存在します。</div>";
+}
 
 
- 
 /**
  * 問２：商品購入
  * 
@@ -42,3 +47,20 @@ $cart = [
     'bed' => 2,
 ];
 echo '<div>商品購入</div>';
+
+foreach ($cart as $c_name => $c_num) {
+
+    echo "<div>{$c_name}を{$c_num}個ください</div>";
+
+    $p_num = $products[$c_name][1];
+
+
+    if ($c_num <= $p_num) {
+        echo "はい。ありがとうございます。";
+    } else {
+        echo "すみません。{$c_name}は{$p_num}個しかありません";
+    }
+
+    //    cartの商品数を[c_num]、商品の数を[p_num]としてif分で条件によって表示を変えるようにしている
+
+}
