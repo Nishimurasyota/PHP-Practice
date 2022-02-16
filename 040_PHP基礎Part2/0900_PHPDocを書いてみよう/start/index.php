2948badebece3b8ab7a9bdeca441e9630964e4b9
nishimurasyota <syota4723@gmail.com>
2022-02-16 22:54:57 +0900

    PHPDocについて

--- 040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
+++ 040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
@@ -1,5 +1,15 @@
 <?php
-function with_tax($base_price, $tax_rate = 0.1) {
+
+/**
+ * 税率計算のための関数を記述するためのファイル
+ */
+
+/**
+ * 税込み金額を取得する関数としてwith_taxを定義
+ */
+
+function with_tax($base_price, $tax_rate = 0.1)
+{
     $sum_price = $base_price + ($base_price * $tax_rate);
     $sum_price = round($sum_price);
     return $sum_price;
