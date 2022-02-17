35f082f43a8c17ffd2f4f947ea44fc14ffb60398
nishimurasyota <syota4723@gmail.com>
2022-02-17 19:59:04 +0900

    三項演算子とnull型演算子について

--- 050_PHP基礎Part3/0100_プログラムの記述順には注意しよう/start/index.php
+++ 050_PHP基礎Part3/0100_プログラムの記述順には注意しよう/start/index.php
@@ -7,5 +7,11 @@ function counter($step = 1)
     return $num;
 }
 
-counter(2);
+// counter(2);
+// $num = 0;
+
+// 上の場合だとcounterがnumよりも先に宣言されているので
+// numが未定義で関数が実行されている、そのため関数ないのnumにはnull(0)が代入されて処理が進む
+
 $num = 0;
+counter(2);
--- 050_PHP基礎Part3/0200_条件分岐を省略して記述してみよう/start/index.php
+++ 050_PHP基礎Part3/0200_条件分岐を省略して記述してみよう/start/index.php
@@ -1,1 +1,25 @@
 <?php
+
+$arr = [
+    "key" => 10,
+];
+
+// if (isset($arr["key"])) {
+//     $arr["key"] *= 10;
+// } else {
+//     $arr["key"] = 1;
+// }
+
+//keyに値があれば10をかけて、ない場合は1を代入する
+
+// $arr["key"] = isset($arr["key"]) ? $arr["key"] * 10 : 1;
+//=以下でif文の内容を三項演算子で表現している
+
+$arr["key"] = $arr["key"] ?? 1;
+//null型演算子
+// $arr["key"]がnullの場合、1が代入される
+// $arr["key"]がnull以外の場合、??の左にある$arr["key"]が代入される
+//三項演算子で表現すると以下のようになる
+//$arr["key"] = isset($arr["key"]) ? $arr["key"] : 1;
+
+echo $arr["key"];
