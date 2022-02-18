afbab5212a4e4aa11ea39052ecc1b0aedb785d78
nishimurasyota <syota4723@gmail.com>
2022-02-18 22:22:44 +0900

    クラス継承の基礎

--- 050_PHP基礎Part3/1000_クラス継承（Part1）/start/index.php
+++ 050_PHP基礎Part3/1000_クラス継承（Part1）/start/index.php
@@ -1,10 +1,15 @@
 <?php
+
+use Japanese as GlobalJapanese;
+use Person as GlobalPerson;
+
 /**
  * クラス継承
  */
 class Person
 {
-    private $name;
+    protected $name;
+    // protectedは自クラスか継承したクラスからの参照を許可する
     public $age;
     public const WHERE = 'Earth';
 
@@ -14,17 +19,30 @@ class Person
         $this->age = $age;
     }
 
-    function hello() {
+    function hello()
+    {
         echo 'hello, ' . $this->name;
         return $this;
     }
 
-    static function bye() {
+    static function bye()
+    {
         echo 'bye';
     }
 }
 
-$bob = new Person('Bob', 18);
+class Japanese extends Person
+//class Japanese extends Personとすることで Personクラスを継承したJapaneseクラスを定義している
+{
+    function hello()
+    {
+        echo 'こんにちは、 ' . $this->name;
+        return $this;
+    }
+    //helloメソッドを変更・上書きして作成している、継承元にあって、継承先にないメソッドやプロパティは引き継ぐ
+}
+
+$bob = new Japanese('翔太', 18);
 $bob->hello();
 // $bob->hello()->bye();
 
