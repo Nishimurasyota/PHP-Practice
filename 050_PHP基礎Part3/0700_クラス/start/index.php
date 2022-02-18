9ace3248e931b03c32d528f7baae64d336f9c678
nishimurasyota <syota4723@gmail.com>
2022-02-18 21:26:41 +0900

    クラスについて

--- 050_PHP基礎Part3/0700_クラス/start/index.php
+++ 050_PHP基礎Part3/0700_クラス/start/index.php
@@ -1,10 +1,23 @@
-<?php 
+<?php
 class Person
 {
-    private $name;
+    public $name;
     public $age;
 
-    function hello() {
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function hello()
+    {
         echo 'hello, ' . $this->name;
     }
 }
+
+$bob = new Person("Bob", 18);
+//echo $bob->name;
+$bob->hello();
+
+// $bob->でbobのメソッドやプロパティを呼び出している
