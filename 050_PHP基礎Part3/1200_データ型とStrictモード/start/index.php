ed09d217841784dc84e7efcb980d222780a12ecc
nishimurasyota <syota4723@gmail.com>
2022-02-18 23:01:59 +0900

    データ型とstrictモードについて

--- 050_PHP基礎Part3/1200_データ型とStrictモード/start/index.php
+++ 050_PHP基礎Part3/1200_データ型とStrictモード/start/index.php
@@ -1,8 +1,14 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+// declare(strict_types=1);で厳密モードに切り替えることができる
 /**
  * データ型とStrictモード
  */
-function add1 (int $val) {
+function add1(int $val): int
+// (int $val)で引数valをint型としている、その後の: intで戻り値の型をint型に指定している
+{
     return $val + 1;
 }
 $result = add1(1);
+var_dump($result);
