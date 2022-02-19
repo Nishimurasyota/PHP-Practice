6198df5e2f25df099db306aab01ef88f203f137f
nishimurasyota <syota4723@gmail.com>
2022-02-20 07:43:05 +0900

    POSTメソッドの書き方について

--- 060_PHPとWEB/0300_POSTメソッドについて学ぼう/start/index.php
+++ 060_PHPとWEB/0300_POSTメソッドについて学ぼう/start/index.php
@@ -1,4 +1,5 @@
-<form action="">
+<form action="post.php" method="POST">
+    <!-- action=""で""に記載しているパス先にリクエストを送信している -->
     <input type="text" name="username">
     <input type="password" name="pwd">
     <input type="submit" value="ボタンを押してね">
