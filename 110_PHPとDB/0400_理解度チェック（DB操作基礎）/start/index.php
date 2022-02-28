da1ba07741f85cd94e31775b2e27702194fca870
nishimurasyota <syota4723@gmail.com>
2022-02-28 23:30:48 +0900

    PHPでのデータ処理（確認問題）

--- 110_PHPとDB/0400_理解度チェック（DB操作基礎）/start/index.php
+++ 110_PHPとDB/0400_理解度チェック（DB操作基礎）/start/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 理解度チェック（DB操作基礎）
  */
@@ -15,23 +16,59 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // 以下の操作を行ってみてください。
 // またtry, catchによってコードを囲んでください。
 
-/**
- * 問１：
- * 店舗Cの全ての商品の在庫数に+10を足し合わせる
- */
+try {
 
+    /**
+     * 問１：
+     * 店舗Cの全ての商品の在庫数に+10を足し合わせる
+     */
 
-/**
- * 問２：
- * 店舗Dを以下のように追加してください。
- */
-/**
- * name: '店舗D'
- * pref_id: 4
- */
+    //     $update_rows = $conn->exec("
+    //  update txn_stocks ts
+    //  set ts.amount = ts.amount + 10
+    //  where ts.shop_id = 3;
+    //  ");
 
-/**
- * 問３：
- * 店舗Aの椅子の在庫数を取得してください。
- */
+    //     echo $update_rows;
 
+    /**
+     * 問２：
+     * 店舗Dを以下のように追加してください。
+     */
+    /**
+     * name: '店舗D'
+     * pref_id: 4
+     */
+
+    //     $inserted_rows = $conn->exec('
+    // insert into mst_shops(name, pref_id,updated_by)
+    // values("店舗D", 4 , "syota");
+    // ');
+
+    //     echo $inserted_rows;
+    /**
+     * 問３：
+     * 店舗Aの椅子の在庫数を取得してください。
+     */
+
+    $p_id = $conn->query("
+     select id from mst_products mp
+     where mp.name = '椅子';
+     ")->fetch()["id"];
+    //  椅子ん商品IDを取得する
+
+    $s_id = $conn->query("
+     select id from mst_shops ms
+     where ms.name = '店舗A';
+     ")->fetch()["id"];
+    //  店舗AのIDを取得する
+
+    $amount = $conn->query("
+     select amount from txn_stocks ts
+     where ts.shop_id = {$s_id}
+     and ts.product_id = {$p_id}
+     ")->fetch()["amount"];
+
+    echo $amount;
+} catch (PDOException $e) {
+}
