a1f433068d73ec3df3f518964e3f0c942039655a
nishimurasyota <syota4723@gmail.com>
2022-02-28 22:45:56 +0900

    例外処理についてver2

--- 110_PHPとDB/0300_例外処理（Part2）/start/index.php
+++ 110_PHPとDB/0300_例外処理（Part2）/start/index.php
@@ -1,29 +1,38 @@
 <?php
+
 /**
  * 例外処理
  */
+
+function throwException()
+{
+    try {
+
+        $bool = false;
+        $bool->method();
+
+        new PDO('', '', '');
+
+        echo '通常処理が最後まで実行されました。<br>';
+    } catch (PDOException $e) {
+
+        echo 'PDOException<br>';
+        echo '例外処理が実行されました。<br>';
+        echo $e->getMessage() . '<br>';
+        // エラーの内容を確認する時にgetMessage()を活用する
+    }
+}
+
 try {
 
-    // new PDO('', '', '');
-    
-    // $bool = false;
-    // $bool->method();
-    
+    throwException();
+
     echo '通常処理が最後まで実行されました。<br>';
-
-} catch(PDOException $e) {
-
-    echo 'PDOException<br>';
-    echo '例外処理が実行されました。<br>';
-    echo $e->getMessage() . '<br>';
-
-} catch(Error $e) {
+} catch (Error $e) {
 
     echo '例外処理が実行されました。<br>';
     echo $e->getMessage() . '<br>';
-
 } finally {
 
     echo '終了処理が実行されました。<br>';
-
 }
