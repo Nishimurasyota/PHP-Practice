47b857830ea9a16003b759ff1fc49ade135e4053
nishimurasyota <syota4723@gmail.com>
2022-02-28 22:00:48 +0900

    PHPでDBの情報を取得する

--- 110_PHPとDB/0100_DBから値を取得/start/index.php
+++ 110_PHPとDB/0100_DBから値を取得/start/index.php
@@ -8,10 +8,11 @@ $dsn = "mysql:host={$host};port=3306;dbname={$dbName}";
 $conn = new PDO($dsn, $user, $pwd);
 $pst = $conn->query("select * from mst_shops");
 $res = $pst->fetchAll(PDO::FETCH_ASSOC);
-// 連装配列で取得するためにfetchAllの第一引数にPDO::FETCH_ASSOCとする
+// 連装配列を取得するためにfetchAllの第一引数にPDO::FETCH_ASSOCとする
 
 echo "<pre>";
 print_r($res);
 echo "</pre>";
 
 $conn = null;
+//コネクションを繋いだら最後は破棄する言語もある
