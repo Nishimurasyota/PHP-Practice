06442fe6e13dc5cc4745d3c9ce1802826cf1a7e8
nishimurasyota <syota4723@gmail.com>
2022-03-01 21:17:15 +0900

    トランザクションについて

--- 110_PHPとDB/0700_トランザクション/start/index.php
+++ 110_PHPとDB/0700_トランザクション/start/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * トランザクション
  * 
@@ -16,9 +17,40 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
+    $product_id = 3;
+    $from_shop_id = 1;
+    $to_shop_id = 3;
+    $amount = 10;
+
+    $conn->beginTransaction();
+    // トランザクションをスタートする
+
+    $pst = $conn->prepare("update txn_stocks
+    set amount = amount + :amount
+    where shop_id = :shop_id
+    and product_id = :product_id
+    ");
+
+    // 商品の数を足す
+    $pst->execute([
+        ":amount" => $amount,
+        ":shop_id" => $to_shop_id,
+        ":product_id" => $product_id
+    ]);
+
+    // 商品の数を減らす
+    $pst->execute([
+        ":amount" => -1 * $amount,
+        ":shop_id" => $from_shop_id,
+        ":product_id" => $product_id
+    ]);
+
+    $conn->commit();
+    // 正常に動作が終わったらcommitでトランザクションを終了してテーブルの情報を更新する
 } catch (PDOException $e) {
 
     echo 'エラーが発生しました。<br>';
     echo $e->getMessage();
-   
+    $conn->rollBack();
+    // エラーが起こったらrollbackしてトランザクションを終了する
 }
