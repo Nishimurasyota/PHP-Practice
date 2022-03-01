000675296db85dc2eed581eb262f3622a70d0491
nishimurasyota <syota4723@gmail.com>
2022-03-01 23:19:46 +0900

    確認問題

--- 110_PHPとDB/0900_理解度チェック（画面とDB操作）/start/index.php
+++ 110_PHPとDB/0900_理解度チェック（画面とDB操作）/start/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 理解度チェック（画面とDB操作クラス）
  * 
@@ -27,3 +28,26 @@
 </form>
 
 <?php
+require_once "datasource.php";
+
+use db\DataSource;
+
+if (isset($_POST["product_id"])) {
+    try {
+        $product_id = $_POST["product_id"];
+
+        $db = new DataSource();
+
+        $result = $db->selectOne("
+select * from mst_products where id = :id and delete_flg != 1;
+", [":id" => $product_id]);
+
+        if (!empty($result)) {
+            echo "商品名は[{$result["name"]}]です。";
+        } else {
+            echo "一致する商品が見つかりません。";
+        }
+    } catch (PDOException $e) {
+        echo "時間をおいて再度お試しください。";
+    }
+}
