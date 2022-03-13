3bc6937e8a61fe023ce11e24d0356f76c278cccc
nishimurasyota <syota4723@gmail.com>
2022-03-13 10:39:50 +0900

    JSでもバリデーションについて

--- 150_JS実践/0200_JSでのFormバリデーション/start/js/form-validate.js
+++ 150_JS実践/0200_JSでのFormバリデーション/start/js/form-validate.js
@@ -1,9 +1,28 @@
-const init = () =>{
+const init = () => {
   const $input = document.querySelector(".validate-target");
-  $input.addEventListener("input",function(){
-      alert("値が変更されました");
-  })
-  console.log($input);
+  $input.addEventListener("input", function (event) {
+    const $target = event.currentTarget;
+    console.log($target.validity);
+    if ($target.validity.valueMissing) {
+      alert("値の入力が必須です");
+    } else if ($target.validity.tooShort) {
+      alert(
+        $target.minLength +
+          "文字以上入力してください。現在の文字数は" +
+          $target.value.length +
+          "文字です"
+      );
+    } else if ($target.validity.tooLong) {
+      alert(
+        $target.maxLength +
+          "文字以下で入力してください。現在の文字数は" +
+          $target.value.length +
+          "文字です"
+      );
+    } else if ($target.validity.patternMismatch) {
+      alert("半角英数字で入力してください");
+    }
+  });
 };
 
 init();
