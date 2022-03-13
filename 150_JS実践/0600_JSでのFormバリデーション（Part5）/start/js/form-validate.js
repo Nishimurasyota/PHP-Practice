9f29291f1a11940da74ade0acf04be1909e1d6ed
nishimurasyota <syota4723@gmail.com>
2022-03-13 16:16:56 +0900

    復習用ディレクトリ作成

--- 150_JS実践/0600_JSでのFormバリデーション（Part5）/start/js/form-validate.js
+++ 150_JS実践/0600_JSでのFormバリデーション（Part5）/start/js/form-validate.js
@@ -1,34 +1,48 @@
 init();
 function init() {
-    const $inputs = document.querySelectorAll('.validate-target');
+  const $inputs = document.querySelectorAll(".validate-target");
+  const $form = document.querySelector(".validate-form");
 
-    for(const $input of $inputs) {
-        
-        $input.addEventListener('input', function(event) {
-            const $target = event.currentTarget;
-            console.log($target.validity);
-            if($target.checkValidity()) {
-    
-                $target.classList.add('is-valid');
-                $target.classList.remove('is-invalid');
-    
-            } else {
-    
-                $target.classList.add('is-invalid');
-                $target.classList.remove('is-valid');
-    
-                if($target.validity.valueMissing) {
-                    console.log('値の入力が必須です。');
-                } else if ($target.validity.tooShort) {
-                    console.log($target.minLength + '文字以上で入力してください。現在の文字数は' + $target.value.length +'文字です。');
-                } else if ($target.validity.tooLong) {
-                    console.log($target.maxLength + '文字以下で入力してください。現在の文字数は' + $target.value.length +'文字です。');
-                } else if ($target.validity.patternMismatch) {
-                    console.log('半角英数字で入力してください。');
-                } 
-            }
-            
-        });
-    }
-    
+  for (const $input of $inputs) {
+    $input.addEventListener("input", function (event) {
+      const $target = event.currentTarget;
+      const $feedback = $target.nextElementSibling;
+
+      if ($target.checkValidity()) {
+        $target.classList.add("is-valid");
+        $target.classList.remove("is-invalid");
+      } else {
+        $target.classList.add("is-invalid");
+        $target.classList.remove("is-valid");
+
+        if ($target.validity.valueMissing) {
+          $feedback.textContent = "値の入力が必須です。";
+        } else if ($target.validity.tooShort) {
+          $feedback.textContent =
+            $target.minLength +
+            "文字以上で入力してください。現在の文字数は" +
+            $target.value.length +
+            "文字です。";
+        } else if ($target.validity.tooLong) {
+          $feedback.textContent =
+            $target.maxLength +
+            "文字以下で入力してください。現在の文字数は" +
+            $target.value.length +
+            "文字です。";
+        } else if ($target.validity.patternMismatch) {
+          $feedback.textContent = "半角英数字で入力してください。";
+        }
+      }
+    });
+  }
+  activateSubmitBtn($form);
+}
+
+function activateSubmitBtn($form) {
+  const $submitBtn = $form.querySelector('[type="submit"]');
+  if ($form.checkValidity()) {
+    $submitBtn.removeAttribute("disabled");
+  } else {
+    $submitBtn.setAttribute("disabled", true);
+  }
 }
