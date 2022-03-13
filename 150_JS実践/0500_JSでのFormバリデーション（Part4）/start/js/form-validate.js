5de61fdca499b2c11941c57dea5313756563b912
nishimurasyota <syota4723@gmail.com>
2022-03-13 11:15:24 +0900

    querySelectorAllとfor文を使用した処理について

--- 150_JS実践/0500_JSでのFormバリデーション（Part4）/start/js/form-validate.js
+++ 150_JS実践/0500_JSでのFormバリデーション（Part4）/start/js/form-validate.js
@@ -1,30 +1,39 @@
 init();
 function init() {
-    const $input = document.querySelector('.validate-target');
-    $input.addEventListener('input', function(event) {
-        const $target = event.currentTarget;
-        console.log($target.validity);
-        if($target.checkValidity()) {
+  const $inputs = document.querySelectorAll(".validate-target");
+  // クラス名にvalidate-targetがついているもの全てをAllで見つけてinputsに格納する
+  for (const $input of $inputs) {
+    // inputsの中からinputを取り出しfor文で処理を行う
+    $input.addEventListener("input", function (event) {
+      const $target = event.currentTarget;
+      console.log($target.validity);
+      if ($target.checkValidity()) {
+        $target.classList.add("is-valid");
+        $target.classList.remove("is-invalid");
+      } else {
+        $target.classList.add("is-invalid");
+        $target.classList.remove("is-valid");
 
-            $target.classList.add('is-valid');
-            $target.classList.remove('is-invalid');
-
-        } else {
-
-            $target.classList.add('is-invalid');
-            $target.classList.remove('is-valid');
-
-            if($target.validity.valueMissing) {
-                console.log('値の入力が必須です。');
-            } else if ($target.validity.tooShort) {
-                console.log($target.minLength + '文字以上で入力してください。現在の文字数は' + $target.value.length +'文字です。');
-            } else if ($target.validity.tooLong) {
-                console.log($target.maxLength + '文字以下で入力してください。現在の文字数は' + $target.value.length +'文字です。');
-            } else if ($target.validity.patternMismatch) {
-                console.log('半角英数字で入力してください。');
-            } 
+        if ($target.validity.valueMissing) {
+          console.log("値の入力が必須です。");
+        } else if ($target.validity.tooShort) {
+          console.log(
+            $target.minLength +
+              "文字以上で入力してください。現在の文字数は" +
+              $target.value.length +
+              "文字です。"
+          );
+        } else if ($target.validity.tooLong) {
+          console.log(
+            $target.maxLength +
+              "文字以下で入力してください。現在の文字数は" +
+              $target.value.length +
+              "文字です。"
+          );
+        } else if ($target.validity.patternMismatch) {
+          console.log("半角英数字で入力してください。");
         }
-        
+      }
     });
-    console.dir($input);
+  }
 }
