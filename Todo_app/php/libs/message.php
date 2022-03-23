406f314c15d2cbc4e5a50ee4ba1d7d1519deb785
nishimurasyota <syota4723@gmail.com>
2022-03-23 21:34:24 +0900

    :zap: エラーメッセージを格納する関数を定義

--- Todo_app/php/libs/message.php
+++ Todo_app/php/libs/message.php
@@ -6,4 +6,37 @@ use model\AbstractModel;
 
 class Msg extends AbstractModel
 {
+    protected static $SESSION_NAME = "_msg";
+
+    public const ERROR = "error";
+    public const INFO = "info";
+    public const DEBUG = "debug";
+
+    /**
+     * エラーメッセージを格納する関数
+     *
+     * @param [type] $type
+     * @param [type] $msg
+     * @return void
+     */
+    public static function push($type, $msg)
+    {
+        if (!is_array(static::getSession())) {
+            static::init();
+        }
+
+        $msgs = static::getSession();
+        $msgs[$type][] = $msg;
+
+        static::setSession($msgs);
+    }
+
+    private static function init()
+    {
+        static::setSession([
+            static::ERROR => [],
+            static::INFO => [],
+            static::DEBUG => []
+        ]);
+    }
 }
