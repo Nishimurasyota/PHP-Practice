1d69ab240b74e553baccbee8670db3fbbdf2fecf
nishimurasyota <syota4723@gmail.com>
2022-03-24 19:41:52 +0900

    :adhesive_bandage: auth.phpの例外処理を記述

--- Todo_app/php/libs/auth.php
+++ Todo_app/php/libs/auth.php
@@ -4,24 +4,32 @@ namespace lib;
 
 use db\UserQuery;
 use model\UserModel;
+use throwable;
 
 class Auth
 {
     public static function login($id, $pwd)
     {
-        $is_success = false;
+        try {
+            $is_success = false;
 
-        $user = UserQuery::fetchById($id);
+            $user = UserQuery::fetchById($id);
 
-        if (!empty($user) && $user->del_flg !== 1) {
-            if (password_verify($pwd, $user->pwd)) {
-                $is_success = true;
-                UserModel::setSession($user);
+            if (!empty($user) && $user->del_flg !== 1) {
+                if (password_verify($pwd, $user->pwd)) {
+                    $is_success = true;
+                    UserModel::setSession($user);
+                } else {
+                    echo "パスワードが一致しません";
+                }
             } else {
-                echo "パスワードが一致しません";
+                echo "IDが一致しません";
             }
-        } else {
-            echo "IDが一致しません";
+        } catch (Throwable $e) {
+
+            $is_success = false;
+            Msg::push(Msg::DEBUG, $e->getMessage());
+            Msg::push(Msg::ERROR, "ログイン処理でエラーが発生しました");
         }
 
         return $is_success;
@@ -29,34 +37,49 @@ class Auth
 
     public static function regist($user)
     {
-        $is_success = false;
+        try {
+            $is_success = false;
 
-        $exist_user = UserQuery::fetchById($user->id);
+            $exist_user = UserQuery::fetchById($user->id);
 
-        if (!empty($exist_user)) {
-            echo "ユーザーが既に存在します";
-            return false;
+            if (!empty($exist_user)) {
+                echo "ユーザーが既に存在します";
+                return false;
+            }
+
+            $is_success = UserQuery::insert($user);
+
+            if ($is_success) {
+                UserModel::setSession($user);
+            }
+        } catch (throwable $e) {
+
+            $is_success = false;
+            Msg::push(Msg::DEBUG, $e->getMessage());
+            Msg::push(Msg::ERROR, "登録処理でエラーが発生しました");
         }
-
-        $is_success = UserQuery::insert($user);
-
-        if ($is_success) {
-            UserModel::setSession($user);
-        }
-
 
         return $is_success;
     }
 
 
-    public static function isLogin(){
-        $user = UserModel::getSession();
+    public static function isLogin()
+    {
+        try {
 
-        if(isset($user)){
+            $user = UserModel::getSession();
+        } catch (throwable $e) {
+
+            UserModel::clearSession();
+            Msg::push(Msg::DEBUG, $e->getMessage());
+            Msg::push(Msg::ERROR, "エラーが発生しました。再度ログインするようにしてください。");
+            return false;
+        }
+
+        if (isset($user)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-
 }
