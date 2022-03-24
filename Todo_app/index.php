70a2e6f89a371ff73e54d5d1ac329c496ef22d86
nishimurasyota <syota4723@gmail.com>
2022-03-24 19:52:55 +0900

    :adhesive_bandage: index.phpni
    indexに例外処理を記述、修正

--- Todo_app/index.php
+++ Todo_app/index.php
@@ -19,16 +19,20 @@ use function lib\route;
 
 session_start();
 
-require_once SOURCE_BASE . "partials/header.php";
-
-$rpath = str_replace(BASE_CONTEXT_PATH, "", (CURRENT_URI));
-// str_replace()で第一引数を第二引数に置換する、対象となる値を第三引数に記載する
-// 今回では$_SERVER["REQUEST_URI"]内でBASE_CONTEXT_PATHと一致する部分を空文字に置換して$rpathに格納している
-
-$method = strtolower($_SERVER["REQUEST_METHOD"]);
-
 use lib\Msg;
 
-route($rpath, $method);
+try {
+    require_once SOURCE_BASE . "partials/header.php";
 
-require_once SOURCE_BASE . "partials/footer.php";
+    $rpath = str_replace(BASE_CONTEXT_PATH, "", (CURRENT_URI));
+    // str_replace()で第一引数を第二引数に置換する、対象となる値を第三引数に記載する
+    // 今回では$_SERVER["REQUEST_URI"]内でBASE_CONTEXT_PATHと一致する部分を空文字に置換して$rpathに格納している
+
+    $method = strtolower($_SERVER["REQUEST_METHOD"]);
+
+    route($rpath, $method);
+
+    require_once SOURCE_BASE . "partials/footer.php";
+} catch (throwable $e) {
+    die("何かがすごくおかしいようです");
+}
