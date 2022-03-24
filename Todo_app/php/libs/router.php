1dc2a412555ca33daf751db26de0a3222f059a57
nishimurasyota <syota4723@gmail.com>
2022-03-24 19:34:21 +0900

    :zap: index.phpのroute関数を別ファイルで定義

---
+++ Todo_app/php/libs/router.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace lib;
+
+use throwable;
+
+function route($rpath, $method)
+{
+    try {
+        if ($rpath === "") {
+            $rpath = "home";
+        }
+
+        $targetFile = SOURCE_BASE . "controllers/{$rpath}.php";
+        // $targetFileに読み込む対象のcontrollerが格納される
+
+        if (!file_exists($targetFile)) {
+            require_once SOURCE_BASE . "/views/404.php";
+            return;
+        }
+        // $targetFileがない場合は404ページにリンクするようにして、returnで以降の処理を行わないようにする
+
+        require_once $targetFile;
+
+
+        $fn = "\\controller\\{$rpath}\\{$method}";
+        $fn();
+    } catch (throwable $e) {
+        Msg::push(Msg::DEBUG, $e->getMessage());
+        Msg::push(Msg::ERROR, "何かがおかしいようです");
+        redirect("404");
+    }
+}
